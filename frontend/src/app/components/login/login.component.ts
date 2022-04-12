bb38f01433702dfebf33c9284fd345e65d980b8e
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 00:24:38 +0530

    nake form

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public form={
+    'email': null,
+    'password':null
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(){}
 
 }
